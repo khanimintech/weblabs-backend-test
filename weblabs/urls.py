6d1ce6cfe91a2870dce6cf70be58aef3d57734a5
khanim Tech <khanimintech@gmail.com>
2022-05-14 10:18:09 +0400

    allprojects, pagination added

--- weblabs/urls.py
+++ weblabs/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home,name="home"),
-    path('about', views.about,name="about")
-
+    path('about', views.about,name="about"),
+    path('allprojects', views.all_projects,name="all_projects")
 ] 
--- weblabs/views.py
+++ weblabs/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from .models import Portfolio
+from django.core.paginator import Paginator
 
 def home(request):
     projects=Portfolio.objects.order_by('-id')
     last_projects=projects[0:6]
-
     context={
         'projects':projects,
         'last_projects':last_projects
@@ -13,3 +13,13 @@ def home(request):
 
 def about(request):
     return render (request, 'about.html')
+
+def all_projects(request):
+    number=request.GET.get('page')
+    projects=Portfolio.objects.order_by('-created_date')
+    projects=Paginator(projects, 4)
+    context={
+        'projects':projects.get_page(number)
+    }
+    return render(request, 'projects.html',context)
+
